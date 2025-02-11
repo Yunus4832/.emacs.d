cd0149c493512cf683267f7aa6a57c76def85c43
yunus0712 <2794914797@qq.com>
2025-02-11 10:50:36 +0800

    feat: 精简 powerline 默认的 mode-line

--- lisp/appearance.el
+++ lisp/appearance.el
@@ -20,10 +20,68 @@
                             (bookmarks . 5)))      ;; 项目显示5条
     (dashboard-setup-startup-hook))
 
+;; powerline 简化主题
+(defun powerline-simplify-theme ()
+  "Setup the simplify mode-line."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face0 (if active 'powerline-active0 'powerline-inactive0))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (separator-left (intern (format "powerline-%s-%s"
+                                                          (powerline-current-separator)
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           (powerline-current-separator)
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list ;; (powerline-raw "%*" face0 'l)
+                                     ;; (when powerline-display-buffer-size
+                                     ;;   (powerline-buffer-size face0 'l))
+                                     ;; (when powerline-display-mule-info
+                                     ;;   (powerline-raw mode-line-mule-info face0 'l))
+                                     (powerline-buffer-id `(mode-line-buffer-id ,face0) 'l)
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format face0 'l))
+                                     (powerline-raw " " face0)
+                                     (funcall separator-left face0 face1)
+                                     (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
+                                       (powerline-raw erc-modified-channels-object face1 'l))
+                                     (powerline-major-mode face1 'l)
+                                     ;; (powerline-process face1)
+                                     ;; (powerline-minor-modes face1 'l)
+                                     (powerline-narrow face1 'l)
+                                     (powerline-raw " " face1)
+                                     (funcall separator-left face1 face2)
+                                     (powerline-vc face2 'r)
+                                     (when (bound-and-true-p nyan-mode)
+                                       (powerline-raw (list (nyan-create)) face2 'l))))
+                          (rhs (list (powerline-raw global-mode-string face2 'r)
+                                     (funcall separator-right face2 face1)
+                                     (unless window-system
+                                       (powerline-raw (char-to-string #xe0a1) face1 'l))
+                                     (powerline-raw "%4l" face1 'l)
+                                     (powerline-raw ":" face1 'l)
+                                     (powerline-raw "%3c" face1 'r)
+                                     (funcall separator-right face1 face0)
+                                     (powerline-raw " " face0)
+                                     (powerline-raw "%6p" face0 'r)
+                                     (when powerline-display-hud
+                                       (powerline-hud face0 face2))
+                                     (powerline-fill face0 0)
+                                     )))
+                     (concat (powerline-render lhs)
+                             (powerline-fill face2 (powerline-width rhs))
+                             (powerline-render rhs)))))))
+
 ;; 使用 Powerline 状态栏
 (use-package powerline
     :config
-    (powerline-default-theme))
+    (powerline-simplify-theme))
 
 ;; 使用 awesome-tab
 (require 'awesome-tab)
@@ -36,6 +94,7 @@
   (let ((name (format "%s" buffer-name)))
     (or
      (string-match "Treemacs" name)
+     (string-match "magit" name)
      (string-match "dashboard" name)
      (string-match "message" name)
      (string-prefix-p "*epc" name)
