e9ba22ab239fb9b794e3161461fda27fec8cde17
yunus0712 <2794914797@qq.com>
2025-09-25 19:11:26 +0800

    feat: 修复 tui 函数未定义

--- early-init.el
+++ early-init.el
@@ -8,14 +8,12 @@
 (menu-bar-mode -1)
 
 ;; 关闭滚动条
-(scroll-bar-mode -1)
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
 
 ;; 关闭工具栏
-(tool-bar-mode -1)
-
-;; 设置字体
-(set-face-attribute 'default nil :family "Consolas NF" :height 120)
-(set-fontset-font t 'han (font-spec :family "新宋体" :height 120))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
 
 ;; 设置默认的窗口列宽 100 列, 背景黑色，前景白色
 (setq default-frame-alist '((width . 100)
