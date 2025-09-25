8ae63385c521964c00acfdfd5f4219d37f473e23
yunus0712 <2794914797@qq.com>
2025-09-25 19:20:14 +0800

    fix: 修复代码问题

--- early-init.el
+++ early-init.el
@@ -16,8 +16,9 @@
   (tool-bar-mode -1))
 
 ;; 设置默认的窗口列宽 100 列, 背景黑色，前景白色
-(setq default-frame-alist '((width . 100)
-                            (background-color . "#272822")
-                            (foreground-color . "white")))
+(when (fboundp 'scroll-bar-mode)
+  (setq default-frame-alist '((width . 100)
+                              (background-color . "#272822")
+                              (foreground-color . "white"))))
 
 ;;; early-init.el ends here
